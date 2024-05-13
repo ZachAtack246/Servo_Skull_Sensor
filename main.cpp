366caf6009df3d367a8a1bc85641f84a4db15d06
ZachAtack246 <123419028+ZachAtack246@users.noreply.github.com>
2024-05-13 16:25:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
+//Importation of necicary utilitys
 #include <iostream>
 #include <string>
 #include <chrono>
 #include <thread>
 #include <unistd.h>
 
+//Initialization of variables
 int servitorsInStock = 7836;
 int constructionsUnderway = 0;
+
+//Main start of program
 void startConstruction(); // Forward declaration
 int main() {
+  //While loop to check servitor var
   while (servitorsInStock < 8000) {
-    ++servitorsInStock;
+    ++servitorsInStock; //Increments of 1 each loop
     std::cout << servitorsInStock << std::endl;
-    sleep(1);
-    if (servitorsInStock == 8000){
+    sleep(1); //Pause for controll
+    if (servitorsInStock == 8000){ //If statement to call startConstruction function
       startConstruction();
     }
   }
 }
+
+//Resets the servitors value to zero and prints text
 void startConstruction() {
   std::cout << "Starting construction...";
   std::cout << constructionsUnderway << std::endl;
